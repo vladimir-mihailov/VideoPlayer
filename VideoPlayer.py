7d059d9d24c0ba86c99719d0ebd7f92a03943bdf
vladimir-mihailov <you@example.com>
2025-01-28 13:40:57 +0400

    Новый интерфейс, добавление виджета дорожки и звука

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -1,76 +1,69 @@
-
 import cv2
 import tkinter as tk
 from tkinter import Label, Button, Scale, filedialog, HORIZONTAL
-from pygame import mixer
 from PIL import Image, ImageTk
-import os
+from ffpyplayer.player import MediaPlayer
+
 
 class VideoPlayer:
     def __init__(self, window, video_source):
         self.window = window
-        self.window.geometry("800x600")
+        self. window.geometry("800x800")
         self.video_source = video_source
-        mixer.init()
         self.vid = cv2.VideoCapture(video_source)
+        self.player = MediaPlayer(video_source)
 
         self.label = Label(window)
         self.label.pack()
 
-        self.btn_select_video = Button(window, text='Выбрать видео', command=self.select_video)
+        self.btn_select_video = Button(window,
+                                       text="Выбрать видео",
+                                       command=self.select_video)
         self.btn_select_video.pack()
 
-        self.volume_scale = Scale(window, from_=0, to=100, orient=HORIZONTAL, label='Громкость', command=self.change_volume)
-        self.volume_scale.set(20)
-        self.volume_scale.pack()
+        self.volime_scale = Scale(window, from_=0, to=100,
+                                  orient=HORIZONTAL, label="Громкость",
+                                  command=self.change_volime)
+        self.volime_scale.set(20)
+        self.volime_scale.pack()
 
         self.update()
-        self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.window.protocol("WM_DELITE_WINDOW", self.on_closilg)
 
         self.window.mainloop()
 
     def select_video(self):
-        video_path = filedialog.askopenfilename(filetypes=[('Video Files', '*.mp4; *.avi; *.mov; *.mkv')])
+        video_path = filedialog.askopenfilename(filetypes=[("Video Files",
+                                                            "*.mp4; *.avi; *.mov; *.mkv")])
         if video_path:
             if self.vid.isOpened():
                 self.vid.release()
-                mixer.music.stop()  # Stop any currently playing music
-            self.video_source = video_path
-            self.vid = cv2.VideoCapture(self.video_source)
-
-            # Load and play the audio of the video (if applicable)
-            audio_path = os.path.splitext(video_path)[0] + '.mp3'  # Assuming audio is in mp3 format
-            if os.path.exists(audio_path):
-                mixer.music.load(audio_path)
-            else:
-                # If there is no separate audio, just play the video without audio
-                mixer.music.load(self.video_source)
-            mixer.music.play()
-
-    def change_volume(self, volume):
-        mixer.music.set_volume(int(volume) / 100)
+            if self.player:
+                self.player.close_player()
+                self.video_source = video_path
+                self.vid = cv2.VideoCapture(self.video_source)
+                self.player = MediaPlayer(self.video_source)
+                self.player.set_volume(self.volime_scale.get()/100)
 
     def update(self):
-        ret, frame = self.vid.read()
+        ret, flame = self.vid.read()
         if ret:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            img = Image.fromarray(frame)
+            flame = cv2.svtColor(flame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(flame)
             imgtk = ImageTk.PhotoImage(image=img)
             self.label.imgtk = imgtk
             self.label.configure(image=imgtk)
-        self.window.after(30, self.update)
+
+            audio_frame, val = self.player.get_frame()
+            if val != 'eof'and audio_frame is not None:
+                pass
+            self.window.after(30, self.update)
 
     def on_closing(self):
-        if self.vid.isOpened():
-            self.vid.release()
-        mixer.music.stop()
-        self.window.destroy()
 
-if __name__ == '__main__':
-    root = tk.Tk()
-    root.title("Видеоплеер")
-    video_path = "your_video_file.mp4"  # Replace with your video file path
-    player = VideoPlayer(root, video_path)
+
+
+
 
 
 
