ceaaf4cdad950894447751c1b2ca0847b90abe27
vladimir-mihailov <you@example.com>
2025-01-28 14:22:51 +0400

    Новый интерфейс, добавление виджета дорожки и звука Нет видео

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -4,7 +4,6 @@ from tkinter import Label, Button, Scale, filedialog, HORIZONTAL
 from PIL import Image, ImageTk
 from ffpyplayer.player import MediaPlayer
 
-
 class VideoPlayer:
     def __init__(self, window, video_source):
         self.window = window
@@ -28,7 +27,7 @@ class VideoPlayer:
         self.volime_scale.pack()
 
         self.update()
-        self.window.protocol("WM_DELITE_WINDOW", self.on_closilg)
+        self.window.protocol("WM_DELITE_WINDOW", self.on_closing)
 
         self.window.mainloop()
 
@@ -45,6 +44,11 @@ class VideoPlayer:
                 self.player = MediaPlayer(self.video_source)
                 self.player.set_volume(self.volime_scale.get()/100)
 
+
+    def change_volime(self, value):
+        if self.player:
+            self.player.set_volume(float(value)/100)
+
     def update(self):
         ret, flame = self.vid.read()
         if ret:
@@ -60,6 +64,18 @@ class VideoPlayer:
             self.window.after(30, self.update)
 
     def on_closing(self):
+        if self.vid.isOpened():
+            self.vid.release()
+        if self.player:
+            self.player.close_player()
+        self.window.destroy()
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Видеоплеер")
+    video_path = "your_video_file.mp4"
+
+player = VideoPlayer(root, video_path)
 
 
 
