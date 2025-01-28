df2c189cc184cf6a456a716fe73e747e19e51d98
vladimir-mihailov <you@example.com>
2025-01-28 15:39:34 +0400

    Создание плеера с меню.

---
+++ Video_Player_Menu.py
@@ -0,0 +1,8 @@
+import cv2
+import tkinter as tk
+from tkinter import Label, Button, Scale, filedialog, HORIZONTAL, messagebox, Menu
+from PIL import Image, ImageTk
+from ffpyplayer.player import MediaPlayer
+import os
+
+class VideoPlayer:
